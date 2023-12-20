98731993096cc00fa0acfc9679d875ab743f94b3
Alfredo <alphaditopeguero@gmail.com>
2023-12-19 19:14:46 -0500

    fix indent

--- api/util.py
+++ api/util.py
@@ -38,18 +38,18 @@ class Attrs:
             setattr(self, key, value)
 
 def topo_sort(graph):
-        in_degree = {u: 0 for u in graph}
-        for u in graph:
-            for v in graph[u]:
-                in_degree[v] += 1
-        queue = deque([u for u in in_degree if in_degree[u] == 0])
-        topo_order = []
-        while queue:
-            u = queue.popleft()
-            topo_order.append(u)
-            for v in graph[u]:
-                in_degree[v] -= 1
-                if in_degree[v] == 0:
-                    queue.append(v)
-        return topo_order
+    in_degree = {u: 0 for u in graph}
+    for u in graph:
+        for v in graph[u]:
+            in_degree[v] += 1
+    queue = deque([u for u in in_degree if in_degree[u] == 0])
+    topo_order = []
+    while queue:
+        u = queue.popleft()
+        topo_order.append(u)
+        for v in graph[u]:
+            in_degree[v] -= 1
+            if in_degree[v] == 0:
+                queue.append(v)
+    return topo_order
 
