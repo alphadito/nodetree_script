c005a7636c3f9c8f1d45d89ff30141b220b784e8
Alfredo <alphaditopeguero@gmail.com>
2023-12-20 21:04:00 -0500

    support scripted expressions in compositor nodetrees

--- api/static/expression.py
+++ api/static/expression.py
@@ -1,6 +1,6 @@
 from ..state import State
 def scripted_expression(scripted_expression: str) -> 'NodeSocket':
-    value_node = State.current_node_tree.new_node('ShaderNodeValue')
+    value_node = State.current_node_tree.new_node(State.NodeSocket.type_to_node[float][0].__name__)
     fcurve = value_node.outputs[0].driver_add("default_value")
     fcurve.driver.expression = scripted_expression
     return State.NodeSocket.create(value_node.outputs[0])
